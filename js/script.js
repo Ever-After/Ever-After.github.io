1f06761c1223fc7c3b6c0a6ddf0cd01c48effede
Ever-After <167075555+Ever-After@users.noreply.github.com>
2024-04-17 10:38:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
-// 目標日時を指定（年, 月（0-11）, 日, 時, 分, 秒）
-const targetDate = new Date( 2025, 2, 24, 0, 0, 0 );
- 
-// カウントダウンタイマー
-const ele = document.getElementById( 'countdown' );
- 
-// カウントダウン処理
-const countdown = () => {
-  // 現在日時
-  const now = new Date();
-  // 時差
-  const distance = targetDate - now;
- 
-  if ( distance < 0 ) {
-    ele.innerHTML = "カウントダウン終了！";
- 
-  } else {
-    const days = Math.floor( distance / ( 1000 * 60 * 60 * 24 ) );
-    const hours = Math.floor( ( distance % ( 1000 * 60 * 60 * 24 ) ) / ( 1000 * 60 * 60 ) );
-    const minutes = Math.floor( ( distance % ( 1000 * 60 * 60 ) ) / ( 1000 * 60 ) );
-    const seconds = Math.floor( ( distance % ( 1000 * 60 ) ) / 1000 );
-    const miliseconds = distance < 0 ? 0 : Math.floor( distance % 1000 );
- 
-    // カウントダウンタイマーのHTML更新
-    ele.innerHTML = `開催まであと<br /><span class="days">${ days }</span>日と<span class="hours">${ String( hours ).padStart( 2, '0' ) }</span>時間<span class="minutes">${ String( minutes ).padStart( 2, '0' ) }</span>分<span class="seconds">${ String( seconds ).padStart( 2, '0' ) }</span>.${ String( miliseconds ).padStart( 3, '0' ) }秒`;
- 
-    // 再度タイマー更新の実行
-    window.requestAnimationFrame( countdown );
-  }
+const daysElm = document.getElementById('days');
+const hoursElm = document.getElementById('hours');
+const minsElm = document.getElementById('mins');
+const secondsElm = document.getElementById('seconds');
+
+// ここに好きな日時を記述する
+// 西暦 月　日
+const target = '2025 2 24';
+
+function countDown() {
+  const targetDate = new Date(target);
+  const currentDate = new Date();
+
+  const totalSeconds = (targetDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysElm.innerHTML = days;
+  hoursElm.innerHTML = formatTime(hours);
+  minsElm.innerHTML = formatTime(mins);
+  secondsElm.innerHTML = formatTime(seconds);
 }
- 
-// カウントダウンタイマーの起動
-window.requestAnimationFrame( countdown );
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countDown, 1000);
