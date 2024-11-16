ca0f92c58f43931e28e29d7a588f07854e26cb53
Ever-After <167075555+Ever-After@users.noreply.github.com>
2024-11-16 16:50:48 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
+//タイマー
+$(function() {
+	var timer = false;
+	$(window).resize(function() {
+		if(timer !== false){
+			clearTimeout(timer);
+		}
+		timer = setTimeout(function() {
+		}, 500);
+	});
+});
 
-// debounce関数
-function debounce(func, wait) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            func.apply(context, args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
-}
+
+// menu
+$(window).on("load resize", function() {
+	setTimeout(function(){
+
+		var winH = window.innerHeight;
+		var winBP = 600;	//ブレイクポイント
+
+			//小さな端末用
+			if(winH <= winBP) {
+				$('body').addClass('s').removeClass('p');
+				$('#menubar').addClass('dn').removeClass('db');
+				$('#menubar_hdr').addClass('db').removeClass('dn').removeClass('ham');
+				
+					// 同一ページへのリンクの場合に開閉メニューを閉じる処理
+					$('#menubar a[href^="#"]').click(function() {
+						$('#menubar').removeClass('db');
+						$('#menubar_hdr').removeClass('ham');
+					});
+					
+			//大きな端末用
+			} else {
+				$('body').addClass('p').removeClass('s');
+				$('#menubar').addClass('db').removeClass('dn');
+				$('#menubar_hdr').removeClass('db').addClass('dn');
+
+			}
+
+	}, 100);
+});
 
 
 //ハンバーガーメニューをクリックした際の処理
@@ -20,169 +48,48 @@ $(function() {
 		$(this).toggleClass('ham');
 
 			if($(this).hasClass('ham')) {
-				$('#menubar,#menubar_hdr_inner').addClass('d-b');
+				$('#menubar').addClass('db').removeClass('dn');
 			} else {
-				$('#menubar,#menubar_hdr_inner').removeClass('d-b');
+				$('#menubar').addClass('dn').removeClass('db');
 			}
 
 	});
 });
 
 
-// 同一ページへのリンクの場合に開閉メニューを閉じる処理
+//pagetop
 $(function() {
-	$('#menubar a[href^="#"]').click(function() {
-		$('#menubar,#menubar_hdr_inner').removeClass('d-b');
-		$('#menubar_hdr,#menubar_hdr_inner').removeClass('ham');
-	});
-});
-
-
-// スムーススクロール（※バージョン2024-1）※通常タイプ
-$(function() {
-    // ページ上部へ戻るボタンのセレクター
-    var topButton = $('.pagetop');
-    // ページトップボタン表示用のクラス名
-    var scrollShow = 'pagetop-show';
-
-    // スムーススクロールを実行する関数
-    // targetにはスクロール先の要素のセレクターまたは'#'（ページトップ）を指定
-    function smoothScroll(target) {
-        // スクロール先の位置を計算（ページトップの場合は0、それ以外は要素の位置）
-        var scrollTo = target === '#' ? 0 : $(target).offset().top;
-        // アニメーションでスムーススクロールを実行
-        $('html, body').animate({scrollTop: scrollTo}, 500);
-    }
-
-    // ページ内リンクとページトップへ戻るボタンにクリックイベントを設定
-    $('a[href^="#"], .pagetop').click(function(e) {
-        e.preventDefault(); // デフォルトのアンカー動作をキャンセル
-        var id = $(this).attr('href') || '#'; // クリックされた要素のhref属性を取得、なければ'#'
-        smoothScroll(id); // スムーススクロールを実行
-    });
-
-    // スクロールに応じてページトップボタンの表示/非表示を切り替え
-    $(topButton).hide(); // 初期状態ではボタンを隠す
-    $(window).scroll(function() {
-        if($(this).scrollTop() >= 300) { // スクロール位置が300pxを超えたら
-            $(topButton).fadeIn().addClass(scrollShow); // ボタンを表示
-        } else {
-            $(topButton).fadeOut().removeClass(scrollShow); // それ以外では非表示
-        }
-    });
-
-    // ページロード時にURLのハッシュが存在する場合の処理
-    if(window.location.hash) {
-        // ページの最上部に即時スクロールする
-        $('html, body').scrollTop(0);
-        // 少し遅延させてからスムーススクロールを実行
-        setTimeout(function() {
-            smoothScroll(window.location.hash);
-        }, 10);
-    }
-});
-
-
-// 汎用開閉処理
-$(function() {
-	$('.openclose').next().hide();
-	$('.openclose').click(function() {
-		$(this).next().slideToggle();
-		$('.openclose').not(this).next().slideUp();
-	});
-});
-
-
-// テキストのフェードイン効果
-$(function() {
-    $('.fade-in-text').on('inview', function(event, isInView) {
-        // この要素が既にアニメーションされたかどうかを確認
-        if (isInView && !$(this).data('animated')) {
-            // アニメーションがまだ実行されていない場合
-            let innerHTML = '';
-            const text = $(this).text();
-            $(this).text('');
-
-            for (let i = 0; i < text.length; i++) {
-                innerHTML += `<span class="char" style="animation-delay: ${i * 0.1}s;">${text[i]}</span>`;
+    var scroll = $('.pagetop');
+    var scrollShow = $('.pagetop-show');
+        $(scroll).hide();
+        $(window).scroll(function() {
+            if($(this).scrollTop() >= 300) {
+                $(scroll).fadeIn().addClass(scrollShow);
+            } else {
+                $(scroll).fadeOut().removeClass(scrollShow);
             }
+        });
+});
 
-            $(this).html(innerHTML).css('visibility', 'visible');
-            // アニメーションが実行されたことをマーク
-            $(this).data('animated', true);
-        }
+
+//スムーススクロール
+$(window).on('load', function() {
+	var hash = location.hash;
+	if(hash) {
+		$('body,html').scrollTop(0);
+		setTimeout(function() {
+			var target = $(hash);
+			var scroll = target.offset().top - 40;
+			$('body,html').animate({scrollTop:scroll},500);
+		}, 100);
+	}
+});
+$(window).on('load', function() {
+    $('a[href^="#"]').click(function() {
+        var href = $(this).attr('href');
+        var target = href == '#' ? 0 : $(href).offset().top - 40;
+            $('body,html').animate({scrollTop:target},500);
+            return false;
     });
 });
 
-
-$(function() {
-    // ロゴの初期のtop位置を取得（ページ上部からの距離）
-    var initialTop = $("#logo").offset().top;
-
-    $(window).scroll(function() {
-        // スクロール量を取得
-        var scrollTop = $(window).scrollTop();
-
-        // #logoのスクロールに応じた位置調整
-        // ここでは、ロゴの初期位置からスクロール量の1/2だけ動かすことで、
-        // デフォルトの位置を基準に動くようにします。
-        // スクロールに応じてロゴが動く速度を調整したい場合は、2の値を変更します。
-        $("#logo").css('top', (initialTop - scrollTop / 2) + 'px');
-    });
-});
-
-
-
-// jsスライド
-const slide = document.getElementById('slide');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const indicator = document.getElementById('indicator');
-const lists = document.querySelectorAll('.list');
-const totalSlides = lists.length;
-let count = 0;
-let autoPlayInterval;
-function updateListBackground() {
-  for (let i = 0; i < lists.length; i++) {
-    lists[i].style.backgroundColor = i === count % totalSlides ? '#fcc800' : '#eee9e6';
-  }
-}
-function nextClick() {
-  slide.classList.remove(`slide${count % totalSlides + 1}`);
-  count++;
-  slide.classList.add(`slide${count % totalSlides + 1}`);
-  updateListBackground();
-}
-function prevClick() {
-  slide.classList.remove(`slide${count % totalSlides + 1}`);
-  count--;
-  if (count < 0) count = totalSlides - 1;
-  slide.classList.add(`slide${count % totalSlides + 1}`);
-  updateListBackground();
-}
-function startAutoPlay() {
-  autoPlayInterval = setInterval(nextClick, 8000);
-}
-function resetAutoPlayInterval() {
-  clearInterval(autoPlayInterval);
-  startAutoPlay();
-}
-next.addEventListener('click', () => {
-  nextClick();
-  resetAutoPlayInterval();
-});
-prev.addEventListener('click', () => {
-  prevClick();
-  resetAutoPlayInterval();
-});
-indicator.addEventListener('click', (event) => {
-  if (event.target.classList.contains('list')) {
-    const index = Array.from(lists).indexOf(event.target);
-    slide.classList.remove(`slide${count % totalSlides + 1}`);
-    count = index;
-    slide.classList.add(`slide${count % totalSlides + 1}`);
-    updateListBackground();
-    resetAutoPlayInterval();
-  }
-});
-startAutoPlay();
